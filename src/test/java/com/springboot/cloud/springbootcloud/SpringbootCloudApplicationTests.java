aee2840c4d533060619ccde8298ac43505fcb4b1
Alexandre Halas <alexandrehalas@gmail.com>
2020-01-23 21:18:48 -0300

    Initial commit

---
+++ src/test/java/com/springboot/cloud/springbootcloud/SpringbootCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.cloud.springbootcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
