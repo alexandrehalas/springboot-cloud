d217fd8ce5f0246694e47569747aa8e49dc9ed23
Alexandre Halas <alexandrehalas@gmail.com>
2020-02-04 23:17:06 -0300

    Created UserResource, and UserNotFoundException

--- src/main/java/com/springboot/cloud/springbootcloud/user/UserResource.java
+++ src/main/java/com/springboot/cloud/springbootcloud/user/UserResource.java
@@ -1,13 +1,18 @@
 package com.springboot.cloud.springbootcloud.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.springboot.cloud.springbootcloud.exception.UserNotFoundException;
 
 @RestController
 public class UserResource {
@@ -22,13 +27,25 @@ public class UserResource {
 	
 	@GetMapping("/users/{id}")
 	public User retrieveUser(@PathVariable int id) {
-		return userDaoService.findOne(id);
+		User user = userDaoService.findOne(id);
+		if (user == null) {
+			throw new UserNotFoundException("id: " + id);
+		}
+		
+		return user;
 	}
 	
 	@PostMapping("/users")
-	public User createUser(@RequestBody User user) {
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
 		User savedUser = userDaoService.save(user);
-		return savedUser;
+		
+		URI location = ServletUriComponentsBuilder
+			.fromCurrentRequest()
+			.path("/{id}")
+			.buildAndExpand(savedUser.getId())
+			.toUri();
+		
+		return ResponseEntity.created(location).build();
 	}
 	
 }
